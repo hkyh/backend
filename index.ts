47c096294f4608522c08d3a7cbd204edad7188fb
KowalewskiPawel <pawelkowalewsk@gmail.com>
2022-11-20 10:42:49 +0100

    cors added

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import { initGame } from "./game";
 import { gameRouter } from "./routes";
 
@@ -9,6 +10,7 @@ const app = express();
 initGame();
 
 app.use(express.json());
+app.use(cors());
 
 app.use('/game', gameRouter);
 
